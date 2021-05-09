534137e4b7aead025d7949d53043aacf79cb4dc8
sxh <steve.e.hayes@gmail.com>
2021-05-09 14:40:45 +0100

    add page button

--- src/UiBase/Buttons.elm
+++ src/UiBase/Buttons.elm
@@ -1,17 +1,18 @@
-module UiBase.Buttons exposing (actionButton, mediumActionButton, smallActionButton)
+module UiBase.Buttons exposing (actionButton, mediumActionButton, smallActionButton, pageSelectButton)
 
 {-| Provide clickable buttons
 
 
 # Buttons
 
-@docs actionButton, mediumActionButton, smallActionButton
+@docs actionButton, mediumActionButton, smallActionButton, pageSelectButton
 
 -}
 
-import Element exposing (Element, padding, text)
+import Element exposing (Element, alignRight, el, mouseOver, padding, text)
 import Element.Background as Background
 import Element.Border exposing (rounded)
+import Element.Events exposing (onClick)
 import Element.Font as Font
 import Element.Input
 import UiBase.Colors exposing (navBackground, navTextColour)
@@ -62,3 +63,10 @@ sizedActionButton event buttonLabel fontSize paddingSize =
         { onPress = Just event
         , label = text buttonLabel
         }
+
+{-| Button for right hand top menu that selects a page
+-}
+pageSelectButton : msg -> String -> Element msg
+pageSelectButton event buttonLabel =
+    el [ Font.size 16, alignRight, padding 8, onClick event, rounded 10, mouseOver [ Background.color navBackground ] ] (text buttonLabel)
+
