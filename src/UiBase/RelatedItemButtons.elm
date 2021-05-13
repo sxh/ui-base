bd7972e4e6aa13ef17453191f895fe8d2a78b150
sxh <steve.e.hayes@gmail.com>
2021-05-13 16:02:41 +0100

    add link buttons

---
+++ src/UiBase/RelatedItemButtons.elm
@@ -0,0 +1,50 @@
+module UiBase.RelatedItemButtons exposing (relatedItemButton, searchResultButton)
+{-| Provide consistent clickable buttons for search and related items
+
+# Buttons
+
+@docs searchResultButton, relatedItemButton
+
+-}
+
+import Colors.Opaque exposing (royalblue)
+import Element exposing (Element, el, mouseOver, padding, paddingXY, row, text)
+import Element.Background as Background
+import Element.Border exposing (rounded)
+import Element.Events exposing (onClick)
+import Element.Font as Font
+import Element.Input exposing (button)
+import UiBase.AircraftTypes exposing (TypedAircraftData)
+import UiBase.Colors exposing (navBackground, navTextColour)
+import UiBase.Constants exposing (defaultRounding)
+
+
+{-| Search result button
+-}
+searchResultButton : (TypedAircraftData -> msg) -> TypedAircraftData -> Element msg
+searchResultButton event data =
+    -- need to wrap this in a column that has the XY padding
+    row
+        [ Font.size 16
+        , Font.color navBackground
+        , paddingXY 20 2
+        , rounded defaultRounding
+        , Font.color navBackground
+        , mouseOver [ Background.color royalblue ]
+        ]
+        [ button [] { onPress = Just (event data), label = text data.aircraftData.name } ]
+
+
+{-| Related item button
+-}
+relatedItemButton : (TypedAircraftData -> msg) -> TypedAircraftData -> Element msg
+relatedItemButton event data =
+    el
+        [ Font.size 14
+        , Font.color navTextColour
+        , padding 8
+        , onClick (event data)
+        , rounded defaultRounding
+        , Background.color navBackground
+        ]
+        (text data.aircraftData.name)
