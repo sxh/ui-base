339decc1e1fe7e0c059ad46066ab4b47c4085e8d
sxh <steve.e.hayes@gmail.com>
2021-05-14 11:14:48 +0100

    more common results view

--- src/UiBase/RelatedItems.elm
+++ src/UiBase/RelatedItems.elm
@@ -1,16 +1,16 @@
-module UiBase.RelatedItems exposing (relatedItemsListAsRow)
+module UiBase.RelatedItems exposing (relatedItemsListRow, relatedItemsListAsRow)
 
 {-| Provide consistent view of related items
 
 # View methods
 
-@docs relatedItemsListAsRow
+@docs relatedItemsListAsRow, relatedItemsListRow
 
 -}
 
-import Element exposing (Element, fill, padding, spacing, width, wrappedRow)
+import Element exposing (Element, fill, padding, row, spacing, text, width, wrappedRow)
 import Element.Font as Font
-import UiBase.AircraftTypes exposing (TypedAircraftData, TypedAircraftList, toTypedList)
+import UiBase.AircraftTypes exposing (TypedAircraftData, TypedAircraftList, isEmpty, toTypedList)
 import UiBase.RelatedItemButtons exposing (relatedItemButton)
 
 {-| All the related items of a single type as a single row
@@ -22,4 +22,13 @@ relatedItemsListAsRow getRelatedAircraftMsg names =
         |> List.map (relatedItemButton getRelatedAircraftMsg)
         |> wrappedRow [ width fill, padding 5, Font.size 16, spacing 5 ]
 
+{-| Row that contains either all the related items, or text if none
+-}
+relatedItemsListRow : (TypedAircraftData -> msg) -> TypedAircraftList -> Element msg
+relatedItemsListRow getRelatedAircraftMsg typedAircraftList =
+    if isEmpty typedAircraftList then
+        row [] [ text "None" ]
+
+    else
+        relatedItemsListAsRow getRelatedAircraftMsg typedAircraftList
 
