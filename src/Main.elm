6eb7c79c1bc107158050ff19b9110ba1e70e074d
sxh <steve.e.hayes@gmail.com>
2021-05-15 13:50:30 +0100

    Display some mocked data

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,11 @@
 module Main exposing (..)
 
 import Browser
-import Element exposing (layout)
+import Element exposing (column, layout, padding, spacing)
 import Html exposing (Html)
 import UiBase.AircraftDetails exposing (aircraftDetailsColumn)
-import UiBase.AircraftTypes exposing (TypedAircraftData)
+import UiBase.AircraftTypes exposing (AircraftType(..), GenericAircraftData, RelatedToAircraft, TypedAircraftData, TypedAircraftList)
+import Vector3
 
 
 main =
@@ -25,8 +26,41 @@ update msg model =
 view : Maybe TypedAircraftData -> Html Msg
 view _ =
     let
-        data =
-            Nothing
+        supermarine : GenericAircraftData
+        supermarine =
+            GenericAircraftData 0 "Supermarine" "" []
+
+        spitfire : GenericAircraftData
+        spitfire =
+            GenericAircraftData 1 "Spitfire" "" []
+
+        base : TypedAircraftData
+        base =
+            TypedAircraftData ManufacturerType supermarine
+
+        someAircraft : TypedAircraftList
+        someAircraft =
+            TypedAircraftList AircraftType [ spitfire ]
+
+        noVariants : TypedAircraftList
+        noVariants =
+            TypedAircraftList VariantType []
+
+        noCommonNames : TypedAircraftList
+        noCommonNames =
+            TypedAircraftList CommonNameType []
+
+        relatedLists : Vector3.Vector3 TypedAircraftList
+        relatedLists =
+            Vector3.from3 someAircraft noVariants noCommonNames
+
+        relatedAircraftData : RelatedToAircraft
+        relatedAircraftData =
+            RelatedToAircraft base relatedLists
     in
-    --  aircraftDetailsColumn : (TypedAircraftData -> msg) -> Maybe RelatedToAircraft -> Element msg
-    layout [] (aircraftDetailsColumn SelectAircraft data)
+    layout []
+        (column [ padding 30, spacing 40 ]
+            [ aircraftDetailsColumn SelectAircraft (Just relatedAircraftData)
+            , aircraftDetailsColumn SelectAircraft Nothing
+            ]
+        )
